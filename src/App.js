6e3a46dee987fff3cf7d9a53a72c327b3fd6aa6f
Madalina <madalina.zamfira@gmail.com>
2020-11-29 00:53:44 +0100

    create input field

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Draggable from "react-draggable";
 import { v4 as uuidv4 } from "uuid";
-var randomColor = require("randomcolor");
+let randomColor = require("randomcolor");
 
 function App() {
-  return <div className="App"></div>;
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  JSON.parse(localStorage.getItem("items")) || [];
+
+  const keyPress = (event) => {
+    let code = event.keyCode || event.which;
+    if (code === 1) {
+      newItem();
+    }
+  };
+
+  return (
+    <div className="App">
+      <input
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        placeholder="Enter your note here"
+        onKeyPress={(e) => keyPress(e)}
+      />
+      <button onClick={newItem}>ENTER</button>
+    </div>
+  );
 }
 
 export default App;
